b51bf7d1881341f760ad7e9c8792690051a4487b
chaldiran527 <panipatsurvivor@gmail.com>
2023-03-14 13:12:45 -0600

    Nasm homework complete

--- NASM/T4/tarea.asm
+++ NASM/T4/tarea.asm
@@ -34,12 +34,15 @@ section .data
 	longMsjSuma equ $ - msjSuma
 	msjDif db 0xA,'Diferencia de ambos numeros es: '
 	longMsjDif equ $ - msjDif
-	msjError db 0xA,'Usted acaba de ingresar un numero invalido!',0xA
+	msjError db 0xA,'ERROR: Usted acaba de ingresar un numero invalido!',0xA
 	longMsjError equ $ - msjError
 	val1 db 50
 	val2 db 60
 	newLine db 0xA
 	longNewLine equ $ - newLine
+	numstr db '1334',0
+	longNumStr equ $-numstr
+
 section .bss
 	num1 resb 4
 	longNum1 equ $-num1
@@ -52,6 +55,10 @@ section .bss
 	nuevaLinea resb 4
 	caracter resb 2
 	longCaracter equ $-caracter
+	numInt1 resb 4
+	longNumInt1 equ $-numInt1
+	numInt2 resb 4
+	longNumInt2 equ $-numInt2
 
 
 
@@ -62,60 +69,154 @@ _start:
 	;Se pide y se recibe el input de los dos numeros a operar
 	print msj1, longMsj1
 	input num1, longNum1
+
+	;Se alistan los registros a ser usados para la conversion de Ascii a entero
+	xor rbx,rbx
+	xor rcx,rcx
+	mov rsi,num1
+
+
+cicloAtoi1:
+	cmp byte[rsi],10
+	je atoi1End
+
+	cmp byte[rsi],48
+	jb _error
+
+	cmp byte[rsi],57
+	ja _error
+
+	movzx rax, byte[rsi]
+	sub rax,'0'
+	imul rbx,10
+	add rbx,rax
+	inc rsi
+	jmp cicloAtoi1
+
+atoi1End:
+	mov rax,rbx
+	mov [numInt1],rax
+
 	print msj2, longMsj2
 	input num2, longNum2
 
-	mov rax,[num1]
-	sub rax, '0'
-	mov rbx, [num2]
-	sub rbx, '0'
+;nuevo atoi para el segundo numero...
+	xor rbx,rbx
+	xor rcx,rcx
+	mov rsi,num2
 
-	add rax,rbx
-	add rax,'0'
+
+cicloAtoi2:
+
+	cmp byte[rsi],10
+	je atoi2End
+
+	cmp byte[rsi],48
+	jb _error
+
+	cmp byte[rsi],57
+	ja _error
+
+	movzx rax, byte[rsi]
+	sub rax,'0'
+	imul rbx,10
+	add rbx,rax
+	inc rsi
+	jmp cicloAtoi2
+
+atoi2End:
+	mov rsp,rbx
+	add rbx,[numInt1]
+
+	mov rax,rbx
+
 	mov [numSuma],rax
+	mov [numInt2],rsp
 
-	mov rax,99
-	mov rbx,4
-	sub rax,rbx
-;	add rax,'0'
-;	mov [numSuma],rax
 
-;    mov rax, 123         ; integer to convert
-    mov rbx, 10          ; base 10
-    mov rcx, numSuma         ; pointer to the output string
+    mov rbx, 10
+    mov rcx, numSuma
     jmp _itoaLoop
-_itoaLoop:
-    xor rdx, rdx         ; clear edx to use as quotient
-    div rbx            ; divide by 10
-    add rdx, '0'         ; convert remainder to ASCII digit
-    mov [rcx], dl        ; store digit in output string
-    inc rcx              ; move to next character in string
-    cmp rax, 0           ; check if quotient is zero
-    jne _itoaLoop      ; if not, continue loop
 
-;	mov [numSuma],rax
+
+_itoaLoop:
+    xor rdx, rdx         
+    div rbx              ; Dividr rbx   
+    add rdx, '0'         ; sumar 49 para el codigo ascii
+    mov [rcx], dl        ; almacenar el digito en rcx de numSuma
+    inc rcx              ; incrementar el contador de la posicion actual del registro iterado
+    cmp rax, 0           ; verificar si es igual que cero 
+    jne _itoaLoop      ; Si es diferente se continua el loop
+
 
 	print msjSuma,longMsjSuma
 	mov rsi,numSuma
 	add rsi,longNumSuma
-	;mov rdx,longNumSuma
 	jmp _contarReves
-	print numSuma,longNumSuma
-	print newLine,longNewLine
 
-	jmp _exit
 
 _contarReves:
 	cmp rsi,numSuma
-	je _exit
+	je _itoa2
 	dec rsi
 	mov al,[rsi]
 	mov [caracter],al
 	print caracter,longCaracter
 	jmp _contarReves
 
+_itoa2:
+	mov al,[numInt1]
+	mov bl,[numInt2]
+	;Se verifica si el numero 1 es menor que el numero 2
+	cmp al,bl
+	jb _itoa2Num1Menor
+	sub al,bl
+	movzx rax,al
+	mov [numDif],rax
+	mov rbx, 10
+	mov rcx,numDif
+
+	jmp _itoaLoop2
+
+_itoa2Num1Menor:
+	sub bl,al 
+	movzx rax,bl
+	mov [numDif],rax
+	mov rbx,10
+	mov rcx,numDif
+	jmp _itoaLoop2
+
+_itoaLoop2:
+    xor rdx, rdx         
+    div rbx              ; Dividr rbx   
+    add rdx, '0'         ; sumar 49 para el codigo ascii
+    mov [rcx], dl        ; almacenar el digito en rcx de numSuma
+    inc rcx              ; incrementar el contador de la posicion actual del registro iterado
+    cmp rax, 0           ; verificar si es igual que cero 
+    jne _itoaLoop2      ; Si es diferente se continua el loop
+
+
+	print msjDif,longMsjDif
+	mov rsi,numDif
+	add rsi,longNumDif
+	jmp _contarReves2
+
+_contarReves2:
+	cmp rsi,numDif
+	je _exit
+	dec rsi
+	mov al,[rsi]
+	mov [caracter],al
+	print caracter,longCaracter
+	jmp _contarReves2
+
+_error:
+	print msjError,longMsjError
+	jmp _exit
+
 _exit:;Se finaliza el programa
-	print newLine,longNewLine
+	print newLine,longNewLine; Se imprime una nuevalinea para permitir ver bien el texto anterior en la consola
+	print newLine,longNewLine; Se imprime una nuevalinea para permitir ver bien el texto anterior en la consola
 	;Llamada de salida
 	mov rbx,0 			;RBX=codigo de salido al 80 
 	mov rax,1			;RAX=funcion sys_exit() del kernel llama al sistema 
